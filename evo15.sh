b0cccb8261e0260405de44dcf929bbf24e9f3d04
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-09-17 09:05:08 +0800

    s

--- evo15.sh
+++ evo15.sh
@@ -1,1 +1,1 @@
-curl -sf https://raw.githubusercontent.com/xc112lg/lg15/refs/heads/main/a15.sh | bash -s evolution
+curl -sf https://raw.githubusercontent.com/xc112lg/lg15/refs/heads/main/a15work.sh | bash -s crdroid
